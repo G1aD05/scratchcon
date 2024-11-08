a1fcee536859899a908f800153aa7ec9add459ed
Glados <ezrag2000@proton.me>
2024-11-07 19:38:43 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scratchcon',
-    version='1.2.3-beta',
+    version='1.2.4-beta',
     packages=find_packages(),
     install_requires=[
         'requests',
@@ -10,8 +10,8 @@ setup(
         'pyperclip',
         'colorama',
     ],
-    author='G1ad0s', 'webbrowser11'
-    author_email="on67703@gmail.com", "terpstragraham@gmail.com"
+    author='G1ad0s, webbrowser11',
+    author_email="on67703@gmail.com, terpstragraham@gmail.com",
     description='A Python library for interacting with the scratch.mit.edu API',
     long_description="https://github.com/g1ad05/scratchcon",
     url='https://github.com/G1ad0s/scratchcon',
