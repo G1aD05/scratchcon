ceb1a43cab5c890ec5406cdfd78ad5e970b01e2b
G1ad05 <ezrag2000@proton.me>
2024-11-12 20:11:25 -0800

    removed test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from scratchcon.actions import actions, login, conn, common
-
-login.login("-g1ad0s-", "Glados")
-conn.connect_project(1050221749)
-
-actions.load()
-actions.Project().post_comment("Hello")
